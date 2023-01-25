483fee05b90db9bb7b96e7e0159b5f4ddf1e5034
BuildTools <unconfigured@null.spigotmc.org>
2023-01-24 19:24:50 -0500

    Reformatted code and added threading

--- server_handler.py
+++ server_handler.py
@@ -11,10 +11,13 @@ import io
 import numpy as np
 from PIL import Image
 import cv2
+import threading
 
 
-HOST = '192.168.0.17'
+HOST = socket.gethostbyname(socket.gethostname())
 PORT = 10000
+ADDR = (HOST, PORT)
+DISCONNECT = 'BYE BYE!'
 
 def cvt_data_to_img(data):
     load_img = io.BytesIO(data)
@@ -22,7 +25,7 @@ def cvt_data_to_img(data):
     return img
    
 #=====================================================================================================
-#Code temporarily borrowed from http://stupidpythonideas.blogspot.com/2013/05/sockets-are-byte-streams-not-message.html
+#Code borrowed from http://stupidpythonideas.blogspot.com/2013/05/sockets-are-byte-streams-not-message.html
 
 def recv_one_message(conn):
     lengthbuf = recvall(conn, 4)
@@ -39,51 +42,33 @@ def recvall(conn, count):
     return buf
 
 #=====================================================================================================
-    
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-    server.bind((HOST, PORT))
-    server.listen(1)
 
-    conn, addr = server.accept()
-    print(f'Now connected to {addr}!')
-    print(addr)
-
-    tries = 0
-    friends = []
-    video_arr = []
-    
-    while tries < 100:
-        data_array = bytearray()
-        chunks = 0
+def handle_client(conn, addr):
+    connected = True
+    while connected:
+        bytes = recv_one_message(conn) 
+        img_jpeg = Image.open(io.BytesIO(bytes))
+        img_np = np.array(img_jpeg)
         
-        img_b = recv_one_message(conn)
-        image = Image.open(io.BytesIO(img_b))
-        img = np.array(image)
-        
-        video_arr.append(img)
-        print('Left inner loop!')
-        
-        tries += 1
-    
-    height, width, _ = video_arr[1].shape
-    
-    video = cv2.VideoWriter('video.avi', -1, 1, (width, height))
-    
-    for image in video_arr:
-        video.write(image)
-        
-    cv2.destroyAllWindows()
-    video.release()
-    
-        
-        
-       #try:
-
-           #image.show()
-        #except:
-          #print('Failed to display image, an error occured in sending the image!')  
-        
-
+        if bytes == DISCONNECT:
+            connected = False
             
+    conn.close() 
         
-        
+def server_main():
+    server.listen()
+    
+    while True:
+        conn, addr = server.accept()
+        print(f'New connection established with {addr}!')
+        thread = threading.Thread(target=handle_client, args = [conn, addr])
+        thread.start()
+
+    
+if __name__ == '__main__':
+    print('Creating server...')
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+    
+    print('Running server!')
+    server_main() 
