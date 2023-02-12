4f533eddbef60a9b8b1168aeab700c9a768cabda
BuildTools <unconfigured@null.spigotmc.org>
2023-02-12 02:33:26 -0500

    Debugging server to work with combined code

--- server.py
+++ server.py
@@ -10,6 +10,7 @@ import io
 import numpy as np
 from PIL import Image
 import threading
+import time
 
 
 from pathlib import Path
@@ -19,7 +20,7 @@ import cv2
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 10000
 ADDR = (HOST, PORT)
-DISCONNECT = str('BYE BYE!')
+DISCONNECT = str('293293').encode('utf-8')
 WEIGHTS = 'C:/Users/nikit/Desktop/YOLOv7/yolov7/yolov7.pt'
 
 def cvt_data_to_img(data):
@@ -33,11 +34,16 @@ def handle_client(conn, addr):
     
     while True:
         msg_len = conn.recv(8)
+        print(msg_len)
         if msg_len:
-            length = str(msg_len.decode('utf-8'))
-            if length == DISCONNECT:
-                print('Client {addr} disconnected!')
-                break
+            if msg_len == DISCONNECT:
+                msg_ok = '6565'
+                msg_ok = str(msg_ok).encode('utf-8')
+                conn.sendall(msg_ok)
+                
+                print(f'Client {addr} disconnected!')
+                break               
+            length = int(msg_len.decode('utf-8'))
             length = int(length)
         
         msg = b''
@@ -54,8 +60,9 @@ def handle_client(conn, addr):
         #detect(source=img_jpeg)
         cv2.imwrite(f'C:/Users/nikit/Desktop/ESP32-Images/IMG{frame_num}.jpg', img_np)
         #print('Saved new image!')
-       
+
     conn.close() 
+    return
         
 def server_main():
     server.listen()
