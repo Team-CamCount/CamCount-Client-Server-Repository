904220ce7b412ceef8c2f9a206f5044b4ea162e5
Nikita Ostro <nostro2018@fau.edu>
2023-02-15 19:32:26 -0500

    Reorganized code, added battery readings

--- server.py
+++ server.py
@@ -20,7 +20,10 @@ import cv2
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 10000
 ADDR = (HOST, PORT)
-DISCONNECT = str('293293').encode('utf-8')
+DISCONNECT = str('BYEBYE').encode('utf-8')
+BATTERY = str('BATTERY').encode('utf-8')
+
+
 WEIGHTS = 'C:/Users/nikit/Desktop/YOLOv7/yolov7/yolov7.pt'
 
 def cvt_data_to_img(data):
@@ -28,32 +31,51 @@ def cvt_data_to_img(data):
     img = np.load(load_img, allow_pickle=True)
     return img
 
+def handle_msg(conn, length):
+    msg = b''
+    while length > 0:
+        temp = conn.recv(length)
+        if temp:
+            msg += temp
+        length -= len(temp)
+    
+    return msg
+
 def handle_client(conn, addr):
     print(f'New connection established with {addr}!')
     frame_num = 0
-    
     while True:
-        msg_len = conn.recv(8)
-        print(msg_len)
-        if msg_len:
-            if msg_len == DISCONNECT:
-                msg_ok = '6565'
+        init_msg = conn.recv(8)
+        print(f'Frame {frame_num+1} received!')
+        if init_msg:
+            if init_msg == BATTERY:
+                batt_len_msg = conn.recv(8)
+                batt_info_len = int(batt_len_msg.decode('utf-8'))
+                
+                batt_info = handle_msg(conn, batt_info_len)
+                short_err = 'YES' if batt_info[2] else 'NO'
+                disc_err = 'YES' if batt_info[3] else 'NO'
+                
+                print(f'ESP32-CAM with address {addr} battery info: Voltage = {batt_info[0]}mV, Battery% = {batt_info[1]}%, Short Error = {short_err}, Battery Disconnect = {disc_err}')
+                print('Sending battery information to Firebase...')
+                #SEND BATTERY INFO TO FIREBASE HERE
+                print('Battery info sent to Firebase!')
+                continue
+            
+            elif init_msg == DISCONNECT:
+                msg_ok = 'OKOK'
                 msg_ok = str(msg_ok).encode('utf-8')
                 conn.sendall(msg_ok)
                 
                 print(f'Client {addr} disconnected!')
-                break               
-            length = int(msg_len.decode('utf-8'))
-            length = int(length)
+                break
+            
+            else:
+                img_len = int(init_msg.decode('utf-8'))
         
-        msg = b''
-        while length > 0:
-            temp = conn.recv(length)
-            if temp:
-               msg += temp
-            length -= len(temp)  
+        img_msg = handle_msg(conn, img_len)
 
-        img = Image.open(io.BytesIO(msg))
+        img = Image.open(io.BytesIO(img_msg))
         img_np = np.array(img)
         
         frame_num += 1 
